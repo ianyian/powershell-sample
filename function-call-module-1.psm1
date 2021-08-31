b331afd429a3c29cda111da57222a0868f169f55
XJ <ianyian@gmail.com>
2021-08-31 14:41:56 +0800

    update

--- function-call-module-1.psm1
+++ function-call-module-1.psm1
@@ -1,6 +1,16 @@
-function foo2
+
+
+write-host "the powershell module is loading" -ForegroundColor green
+function Get-WelcomeMessage2
 {
     Write-Output "hello world 2"
 }
 
-Export-ModuleMember -Function foo2
+<#
+    launch script
+        import-module .\function-call-module-1.psm1   
+        Get-WelcomeMessage2
+
+        remove and load agian hte module
+        import-module .\function-call-module-1.psm1 -Force -Verbose
+    #>
